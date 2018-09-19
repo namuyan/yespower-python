6eeaa6c1eed38cc39cebcab4162bfb8e9ae63128
namuyang <thhjuu@yahoo.co.jp>
2018-09-19 21:02:03 +0900

    for loop initial declarations

--- yespower.c
+++ yespower.c
@@ -48,7 +48,8 @@ int yespower_hash(const char *input, char *output)
 //        yespower_tls(input, 80, &v1, (yespower_binary_t *) output);
 //    }
     
-    for(int j = 0; j < 16; j++){  // reverse output
+        int j;
+    for(j = 0; j < 16; j++){  // reverse output
         int t = output[j];
         output[j] = output[32- j - 1];
         output[32 - j - 1] = t;
